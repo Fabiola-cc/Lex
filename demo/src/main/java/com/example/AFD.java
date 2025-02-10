d24beb1b2a528a26e52afae8a74baa75ad1efa32
Fabiola Contreras <fcontrerascol@gmail.com>
2025-02-09 19:07:06 -0600

    feat: create AFD transitions logic

--- demo/src/main/java/com/example/AFD.java
+++ demo/src/main/java/com/example/AFD.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.models.RegexToken;
 import com.example.models.node;
@@ -9,39 +10,96 @@ import com.example.models.node;
 import java.util.ArrayList;
 
 public class AFD {
-    private ArrayList<node> tree_info;
-    private ArrayList<node> functions_table;
-    private HashMap<String, List<String>>[] transitions_table;
+    private static ArrayList<node> tree_info;
+    private static HashMap<String, List<String>> transitions_table;
+    private static List<String> States;
+    private static List<Character> Symbols;
 
     public AFD(List<Object> tree) {
-        tree_info = read_tree(tree);
-        ;
-        functions_table = new ArrayList<node>();
+        tree_info = new ArrayList<node>();
+        read_tree(tree);
+        check_calculated_functions();
+        find_symbols();
+
+        transitions_table = new HashMap<String, List<String>>();
+        States = new ArrayList<>();
+        Symbols = new ArrayList<>();
     }
 
-    public static boolean isAlphanumeric(String str) {
-        return str.matches("[a-zA-Z0-9]+");
+    public static ArrayList<node> getTree_info() {
+        return tree_info;
     }
 
-    private ArrayList<node> read_tree(List<Object> tree) {
-
-        return null;
+    private void read_tree(List<Object> tree) {
 
     }
 
-    private void check_calculated_functions() {
-        for (int i = 0; i < tree_info.size(); i++) {
-            node actualNode = tree_info.get(i);
-            if (actualNode.isAlphanumeric()) {
-                actualNode.setNullable(false);
-
-            }
+    private static void check_calculated_functions() {
+        for (node object : tree_info) {
+            object.setNullable(Calculated_functions.isNullable(object));
+        }
+        for (node object : tree_info) {
+            object.setFirstpos(Calculated_functions.getFirstPos(object));
+        }
+        for (node object : tree_info) {
+            object.setLastpos(Calculated_functions.getLastPos(object));
+        }
+        for (node object : tree_info) {
+            Calculated_functions.getFollowPos(object);
         }
 
     }
 
-    private void create_transitions() {
+    private static void create_transitions(int root) {
+        String initialState = tree_info.get(root).getFirstpos().toString().replaceAll("[\\[\\], ]", "");
 
+        List<String> transitions = save_transitions(initialState.toCharArray());
+        transitions_table.put(initialState, transitions);
+
+        state_transitions(transitions);
     }
 
+    private static List<String> save_transitions(char[] state) {
+        Map<Character, List<String>> grupos = new HashMap<>();
+        for (Character actualNode : state) {
+            node n = tree_info.get(getTreeIndex(actualNode.toString()));
+            grupos.computeIfAbsent(n.getValue(), k -> new ArrayList<>())
+                    .add(n.getfollowpos().toString().replaceAll("[\\[\\], ]", ""));
+        }
+        List<String> result = new ArrayList<>();
+        for (Character c : grupos.keySet()) {
+            result.add(grupos.get(c).toString().replaceAll("[\\[\\], ]", ""));
+        }
+        return result;
+    }
+
+    private static void state_transitions(List<String> transitions) {
+        for (String state : transitions) {
+            if (!States.contains(state)) {
+                States.add(state);
+                List<String> actual_transitions = save_transitions(state.toCharArray());
+
+                transitions_table.put(state, actual_transitions);
+                state_transitions(actual_transitions);
+            }
+        }
+    }
+
+    private static void find_symbols() {
+        for (node object : tree_info) {
+            if (object.isAlphanumeric() && !Symbols.contains(object.getValue())) {
+                Symbols.add(object.getValue());
+            }
+        }
+    }
+
+    public static int getTreeIndex(String elementName) {
+        for (node node : getTree_info()) {
+            if (node.getName().equals(elementName)) {
+                return getTree_info().indexOf(node);
+            }
+        }
+        System.err.println("ERROR: no se encontró el nodo dado");
+        return -1;
+    }
 }
