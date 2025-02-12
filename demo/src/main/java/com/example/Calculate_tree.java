717fb7dd9d7d3dd7166a2ee62b3b659f52b40448
Maria-Villafuerte <villafuerte.mariajose046@gmail.com>
2025-02-12 12:29:44 -0600

    feat: creation of new file for tree

---
+++ demo/src/main/java/com/example/Calculate_tree.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.example.models.node;
+
+public class Calculate_tree {
+  
+  // INPUT: List<RegexToken> 
+  // OUTPUT: List<node> 
+  
+
+  /*
+   * Crear una función que toma un List<RegexToken> que nos da la información de si es un operador o no y que valor tiene
+   * Tenemos que regresar una lista de nodos en donde se indique la jerarquia 
+   * 
+   * Proceso para lograrlo: leer primer objeto de Lista input si no es operdor obtener valor y grardarlo en un stack
+   * , lo mismo con lo siguinte hasta que se tope con un operador va a crear un nodo operador 
+   */
+   
+    private static int nodeCounter = 1;
+    private final List<node> treeNodes;
+    private final Stack<node> operandStack;
+
+    public v+Calculate_tree() {
+        this.treeNodes = new ArrayList<>();
+        this.operandStack = new Stack<>();
+    }
+
+}
