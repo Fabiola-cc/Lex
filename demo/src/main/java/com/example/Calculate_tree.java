1d0bce482448c94f70626078b69ed2cd28c98468
Maria-Villafuerte <villafuerte.mariajose046@gmail.com>
2025-02-12 12:36:05 -0600

    update: convertion tree, initial methods

--- demo/src/main/java/com/example/Calculate_tree.java
+++ demo/src/main/java/com/example/Calculate_tree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import com.example.models.RegexToken;
 import com.example.models.node;
 
 public class Calculate_tree {
@@ -13,26 +14,55 @@ public class Calculate_tree {
   
 
   /*
-   * Crear una función que toma un List<RegexToken> que nos da la información de si es un operador o no y que valor tiene
-   * Tenemos que regresar una lista de nodos en donde se indique la jerarquia 
-   * 
-   * Proceso para lograrlo: leer primer objeto de Lista input si no es operdor obtener valor y grardarlo en un stack
-   * , lo mismo con lo siguinte hasta que se tope con un operador va a crear un nodo operador 
-   */
-   
-  //  Asignamos los parámetros a utilizar, la lista de nodos que retornaremos, el nodo en el que nuesencotramos y el stack que nos ayudará a hacer el arbol 
-    private static int nodeCounter = 1;
-    private final List<node> treeNodes;
-    private final Stack<node> operandStack;
-
-    public Calculate_tree() {
-        this.treeNodes = new ArrayList<>();
-        this.operandStack = new Stack<>();
-    }
-
-
-public static void main(String[] args) {
+  * Crear una función que toma un List<RegexToken> que nos da la información de si es un operador o no y que valor tiene
+  * Tenemos que regresar una lista de nodos en donde se indique la jerarquia 
+  * 
+  * Proceso para lograrlo: leer primer objeto de Lista input si no es operdor obtener valor y grardarlo en un stack
+  * , lo mismo con lo siguinte hasta que se tope con un operador va a crear un nodo operador 
+  */
   
-}
+  //  Asignamos los parámetros a utilizar, la lista de nodos que retornaremos, el nodo en el que nuesencotramos y el stack que nos ayudará a hacer el arbol 
+  private static int nodeCounter = 1;
+  private final List<node> treeNodes;
+  private final Stack<node> operandStack;
+
+  public Calculate_tree() {
+      this.treeNodes = new ArrayList<>();
+      this.operandStack = new Stack<>();
+  }
+
+  /**
+   * Método principal que obtiene la lista de nodos y convierte en un árbol
+  */
+
+  public List<node> convertPostfixToTree(List<RegexToken> postfix) {
+    for (RegexToken token : postfix) {
+        if (!token.getIsOperator()) {
+            handleAlfanumerico(token);
+        } else {
+            handleOperator(token);
+        }
+    }
+    return treeNodes;
+  }
+
+  /**
+   * Handles leaf nodes (operands) in the expression
+   */
+  private void handleAlfanumerico(RegexToken token) {
+
+  }
+
+  /**
+  * Handles operator nodes based on their type
+  */
+  private void handleOperator(RegexToken token) {
+
+  }
+
+
+  public static void main(String[] args) {
+    
+  }
 
 }
