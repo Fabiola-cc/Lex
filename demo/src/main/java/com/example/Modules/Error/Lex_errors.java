c7ae55b8bdc17b865f280d1c7ec57675ffdc9916
Fabiola Contreras <fcontrerascol@gmail.com>
2025-03-19 13:09:34 -0600

    feat: Error managment function

---
+++ demo/src/main/java/com/example/Modules/Error/Lex_errors.java
@@ -0,0 +1,19 @@
+package com.example.Modules.Error;
+
+public class Lex_errors extends Exception {
+    private String errorMessage;
+    private int line;
+    private int char_num;
+
+    public Lex_errors(String errorMessage, int line, int char_num) {
+        super(errorMessage);
+        this.errorMessage = errorMessage;
+        this.line = line;
+        this.char_num = char_num;
+    }
+
+    @Override
+    public String toString() {
+        return "Error léxico en la línea " + line + ", en el caracter " + char_num + "\n\t:" + errorMessage;
+    }
+}
