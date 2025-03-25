19f104b35889e400bc05d25c2759668a4bf66cb7
DiegoDuaS <diego.slowing@gmail.com>
2025-03-25 12:44:42 -0600

    fix: input now recognizes comilla

--- demo/src/main/java/com/example/Modules/Regex/RegexConvertor.java
+++ demo/src/main/java/com/example/Modules/Regex/RegexConvertor.java
@@ -1,9 +1,11 @@
 package com.example.Modules.Regex;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class RegexConvertor {
-    private static final Set<String> OPERATORS = Set.of("|", "*", "+", "(", ")", "[", "]", "#", "");
+    private static final Set<String> OPERATORS = Set.of("|", "*", "+", "(", ")", "[", "]", "#", "", "_");
 
     public static Map<String, String> convertRegexMap(Map<String, String> regexToTokenMap) {
         Map<String, String> processedMap = new LinkedHashMap<>();
@@ -20,11 +22,22 @@ public class RegexConvertor {
         StringBuilder result = new StringBuilder();
         boolean insideQuotes = false;
         StringBuilder literalBuffer = new StringBuilder();
+        int quoteCount = 0; 
 
         for (int i = 0; i < regex.length(); i++) {
             char c = regex.charAt(i);
 
             if (c == '\'') {
+                quoteCount += 1; 
+                
+                if (quoteCount == 2) {
+                    result.append("\\'");
+                    quoteCount = 0; 
+                    i = i + 1;
+                    insideQuotes = !insideQuotes;
+                    continue;
+                }
+
                 if (insideQuotes) {
                     String literal = literalBuffer.toString();
                     literalBuffer.setLength(0);
@@ -36,11 +49,14 @@ public class RegexConvertor {
                     } else {
                         result.append(literal);
                     }
+
+                    quoteCount = 0; 
                 }
                 insideQuotes = !insideQuotes;
             } else {
                 if (insideQuotes) {
                     literalBuffer.append(c);
+                    quoteCount = 0; 
                 } else {
                     result.append(c);
                 }
@@ -52,10 +68,10 @@ public class RegexConvertor {
 
     public static void main(String[] args) {
         Map<String, String> regexToTokenMap = new LinkedHashMap<>();
-        regexToTokenMap.put("'+'+", "PLUS");
+        regexToTokenMap.put("['a'-'z''A'-'Z']+'_'['0'-'9']+", "PLUS");
         regexToTokenMap.put("['a'-'z''A'-'Z']#['a'-'b']'['", "INDENT");
         regexToTokenMap.put("' '|'\\t'", "WHITESPACE");
-        regexToTokenMap.put("'t''a''m''a''r''i'", "IDENTIFIER");
+        regexToTokenMap.put("'H'", "IDENTIFIER");
 
         Map<String, String> processedRegexMap = convertRegexMap(regexToTokenMap);
         System.out.println("Mapa procesado: " + processedRegexMap);
